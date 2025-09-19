6a8e30677701b413e5325298eeaef29358e0d2fe
Sanjanadas50 <CHN24Mc070@ceconline.edu>
2025-09-19 14:37:02 +0530

    Create activityOne.java

---
+++ MAD/Cycle3/Optionsmenu/activityOne.java
@@ -0,0 +1,11 @@
+package com.example.optionsmenuapp;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+// This is the first destination activity.
+public class ActivityOne extends AppCompatActivity {
+  @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_one); // Display the layout for this activity.
+ }
+}
